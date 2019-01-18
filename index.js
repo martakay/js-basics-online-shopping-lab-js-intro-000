3725b825abd2d24de843972f8b41f0d898cf5d01
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-18 17:05:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,18 +41,18 @@ function listCartItems() {
   //   }
   // }
   var cartDescription = 'In your cart, you have '
-if ( getCart().length >= 1 ) {
-  cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
-}
-if ( getCart().length >= 2 ) {
-  var middleCartItemsDescription = ''
-  for (var i=1; i<getCart().length -1; i++) {
-    middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
-  }
-  cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
-}
+    if ( getCart().length >= 1 ) {
+      cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
+    }
+    if ( getCart().length >= 2 ) {
+      var middleCartItemsDescription = ''
+      for (var i=1; i<getCart().length -1; i++) {
+        middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+      }
+      cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
+    }
 
-return `${cartDescription}.`
+    return `${cartDescription}.`
 }
 
 function total() {
