3c550b3bc0d386c52e7e127ac81e08d131527a52
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-18 17:35:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ function listCartItems() {
     for ( var i = 1; i < getCart().length - 1 ; i++ ) {
       middleCartMessage += `, ${getCart()[ i ].itemName} at $${getCart()[ i ].itemPrice}`
     }
-    cartMessage += `${middleCartMessage}, and ${getCart()[ i ].itemName} at $${getCart()[ i ].itemPrice}`
+    cartMessage += `${ middleCartMessage}, and ${getCart()[ i ].itemName} at $${getCart()[ i ].itemPrice }`
   }
 
-  return `${cartMessage}.`;
+  return `${ cartMessage }.`;
 }
 
 function total() {
