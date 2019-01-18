43d381ccdd806d73899d81fef2ad0a1988d6de01
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-18 16:13:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemDetails = getItemDetails(item)
+  getCart().push(itemDetails);
+
+  return `${itemDetails.itemName} has been added to your cart.`
+}
+
+function getItemDetails(itemInfo) {
+  return {
+    itemName: itemInfo,
+    itemPrice: Math.floor(Math.random() * 100) + 1
+  }
 }
 
 function viewCart() {
-  // write your code here
+  if (getCart().length < 1) {
+    return "Your shopping cart is empty.";
+  }
+  else if ( getCart().length >= 1 ) {
+    var stringOne = `${getCart()[0].itemName} at $${getCart()[0].itemPrice}.`
+  }
 }
 
 function total() {
