6267c5bdf0e47945107a4ba3da33936417fcbf26
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-18 16:57:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ function viewCart() {
 }
 
 function listCartItems() {
+  var cartMessage = 'In your cart, you have '
+
   if ( getCart().length >=  1 ) {
     var oneItem = `In your cart, you have ${ getCart()[ 0 ].itemName } at ${ getCart()[ 0 ].itemPrice }.`
   }
